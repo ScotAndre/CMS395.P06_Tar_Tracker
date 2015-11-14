db03f66e68de0c6d0304c266df3d495de6027492
ScotAndre <sandre@rollins.edu>
2015-11-14 13:25:34 -0500

    Added NightOut.java shell

---
+++ app/src/main/java/edu/rollins/cms395/tartracker/NightOut.java
@@ -0,0 +1,11 @@
+package edu.rollins.cms395.tartracker;
+
+/*
+ *  The NightOut class will keep track of the things that make a night out
+ *  * Track the start time of the night out
+ *  * Track the places visited and the time stamps of entering and leaving
+ *  each place.
+ *  * Track each drink consumed at each place including the time of the drink
+ */
+public class NightOut {
+}
