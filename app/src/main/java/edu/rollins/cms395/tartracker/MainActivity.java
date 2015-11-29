d7e1d4c795268c5f894e4f46344e58cddab8f7f1
PSKELLY <pskelly@rollins.edu>
2015-11-29 11:02:03 -0500

    added sound, animation, and if statement to onbuttonclick

--- app/src/main/java/edu/rollins/cms395/tartracker/MainActivity.java
+++ app/src/main/java/edu/rollins/cms395/tartracker/MainActivity.java
@@ -2,12 +2,15 @@ package edu.rollins.cms395.tartracker;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.SoundPool;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,6 +28,11 @@ public class MainActivity extends AppCompatActivity {
     //TODO: work on sound effects
     //TODO: auto refresh mainActivity
 
+    private SoundPool soundPool;
+    private int soundBeerPour;
+    private DatabaseManager db;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +43,10 @@ public class MainActivity extends AppCompatActivity {
         bac = new BacCalculator(this);
 
         tvDrinkCount = (TextView) findViewById(R.id.drink_count);
+
+        configSounds();
+
+        db = new DatabaseManager( this );
     }
 
     @Override
@@ -73,6 +85,30 @@ public class MainActivity extends AppCompatActivity {
         bac.addDrink();
         Toast.makeText(this, "# of Drinks = " + bac.getmDrinkCount(), Toast.LENGTH_LONG).show();
         tvDrinkCount.setText("" + bac.getmDrinkCount());
+
+        soundPool.play(soundBeerPour, 1, 1, 1, 0, 1.0f);
+
+        TextView current_state = ( TextView )findViewById( R.id.current_state );
+
+        if ( bac.getmDrinkCount() != 0 ) {
+
+            if ( bac.getBac() < .02 ){
+                performAnimation(R.anim.spin);
+                current_state.setText( "Suprisingly Sober");
+            }
+            else if ( ( bac.getBac() >= .02 ) && ( db.getAge() < 21 ) ){
+                performAnimation(R.anim.combo);
+                current_state.setText( "YOUR DRUNK KID!");
+            }
+            else if ( bac.getBac() >= .08 ){
+                performAnimation(R.anim.spin);
+                current_state.setText( "Your kinda drunk");
+            }
+            else if ( bac.getBac() >= .15 ){
+                performAnimation(R.anim.combo);
+                current_state.setText( "YOUR BEYOND DRUNK!");
+            }
+        }
     }
 
     public void onClickResetButton(View view){
@@ -80,5 +116,33 @@ public class MainActivity extends AppCompatActivity {
         bac.resetDrinkCounter();
         tvDrinkCount.setText("" + bac.getmDrinkCount());
 
+    }
+
+    private void configSounds( ){
+        SoundPool.Builder spb = new SoundPool.Builder( );
+        spb.setMaxStreams( 1 );
+        soundPool = spb.build( );
+        soundBeerPour = soundPool.load( this, R.raw.beer_pour, 1 );
+    }
+
+    public void startRotate ( View v ) {
+        performAnimation(R.anim.spin);
+    }
+
+
+    private void performAnimation( int animationResourceID ) {
+        Animation an = AnimationUtils.loadAnimation(this, animationResourceID);
+        an.setAnimationListener(new MyAnimationListener());
+        TextView current_state = ( TextView )findViewById( R.id.current_state );
+        current_state.startAnimation(an);
+    }
+
+    class MyAnimationListener implements Animation.AnimationListener {
+
+        public void onAnimationStart( Animation animation ) { }
+
+        public void onAnimationEnd( Animation animation ) { }
+
+        public void onAnimationRepeat( Animation animation ) { }
     }
 }
